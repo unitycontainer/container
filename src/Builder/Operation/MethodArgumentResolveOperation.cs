88099ff966ccfc8077c54f2d47a6a4609e151f59
Eugene Sadovoi <evgeni@eniks.com>
2017-10-12 23:30:21 -0400

    Added Enumerable Strategy and tests

---
+++ src/Builder/Operation/MethodArgumentResolveOperation.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.Reflection;
+
+namespace Unity.Builder.Operation
+{
+    /// <summary>
+    /// This class records the information about which constructor argument is currently
+    /// being resolved, and is responsible for generating the error string required when
+    /// an error has occurred.
+    /// </summary>
+    public class MethodArgumentResolveOperation : ParameterResolveOperation
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructorArgumentResolveOperation"/> class.
+        /// </summary>
+        /// <param name="typeBeingConstructed">The type that is being constructed.</param>
+        /// <param name="methodSignature">A string representing the method being called.</param>
+        /// <param name="parameterName">Parameter being resolved.</param>
+        public MethodArgumentResolveOperation(Type typeBeingConstructed, string methodSignature, string parameterName)
+            : base(typeBeingConstructed, parameterName)
+        {
+            MethodSignature = methodSignature;
+        }
+
+        /// <summary>
+        /// Generate the string describing what parameter was being resolved.
+        /// </summary>
+        /// <returns>The description string.</returns>
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentCulture,
+                Constants.MethodArgumentResolveOperation,
+                ParameterName, TypeBeingConstructed.GetTypeInfo().Name, MethodSignature);
+        }
+
+        /// <summary>
+        /// String describing the method being set up.
+        /// </summary>
+        public string MethodSignature { get; }
+    }
+}
