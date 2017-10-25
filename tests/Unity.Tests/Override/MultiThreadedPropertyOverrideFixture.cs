50523dc4fe9f5adcea5a1c9c61b7ac5bc353ae0a
Eugene Sadovoi <evgeni@eniks.com>
2017-10-24 20:08:50 -0400

    Updating references and adding tests

--- tests/Unity.Tests/Override/MultiThreadedPropertyOverrideFixture.cs
+++ tests/Unity.Tests/Override/MultiThreadedPropertyOverrideFixture.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
-using Microsoft.Practices.Unity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Threading;
