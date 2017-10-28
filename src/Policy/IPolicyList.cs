34a2b2c528c2546a3a566ecc30edadbedc0ddba7
Eugene Sadovoi <evgeni@eniks.com>
2017-10-28 15:54:33 -0400

    Promoting Abstractions to 2.1.0

--- src/Policy/IPolicyList.cs
+++ src/Policy/IPolicyList.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 using System;
-using Unity.Builder;
 
 namespace Unity.Policy
 {
@@ -11,23 +10,10 @@ namespace Unity.Policy
     public interface IPolicyList
     {
         /// <summary>
-        /// Removes an individual policy type for a build key.
-        /// </summary>
-        /// <param name="policyInterface">The type of policy to remove.</param>
-        /// <param name="buildKey">The key the policy applies.</param>
-        void Clear(Type policyInterface, object buildKey);
-
-        /// <summary>
-        /// Removes all policies from the list.
-        /// </summary>
-        void ClearAll();
-
-        /// <summary>
         /// GetOrDefault the non default policy.
         /// </summary>
         /// <param name="policyInterface">The interface the policy is registered under.</param>
         /// <param name="buildKey">The key the policy applies to.</param>
-        /// <param name="localOnly">True if the search should be in the local policy list only; otherwise false to search up the parent chain.</param>
         /// <param name="containingPolicyList">The policy list in the chain that the searched for policy was found in, null if the policy was
         /// not found.</param>
         /// <returns>The policy in the list if present; returns null otherwise.</returns>
@@ -40,5 +26,16 @@ namespace Unity.Policy
         /// <param name="policy">The policy to be registered.</param>
         /// <param name="buildKey">The key the policy applies.</param>
         void Set(Type policyInterface, IBuilderPolicy policy, object buildKey = null);
+        /// <summary>
+        /// Removes an individual policy type for a build key.
+        /// </summary>
+        /// <param name="policyInterface">The type of policy to remove.</param>
+        /// <param name="buildKey">The key the policy applies.</param>
+        void Clear(Type policyInterface, object buildKey);
+
+        /// <summary>
+        /// Removes all policies from the list.
+        /// </summary>
+        void ClearAll();
     }
 }
