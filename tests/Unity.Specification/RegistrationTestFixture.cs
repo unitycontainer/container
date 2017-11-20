24e4dc9037d030a208022010960662244c447c3c
Eugene Sadovoi <evgeni@eniks.com>
2017-11-19 20:32:32 -0500

    Added new Specification Tests

---
+++ tests/Unity.Specification/RegistrationTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification
+{
+    [TestClass]
+    public class RegistrationTestFixture : Registration.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer();
+        }
+    }
+}
