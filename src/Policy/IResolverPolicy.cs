e8c2b3865c5bbdee2511bb38e541c81f2aadb6b0
Eugene Sadovoi <evgeni@eniks.com>
2017-11-21 22:45:16 -0500

    Added IContext

--- src/Policy/IResolverPolicy.cs
+++ src/Policy/IResolverPolicy.cs
@@ -1,16 +1,17 @@
 ﻿using Unity.Builder;
+using Unity.Resolution;
 
 namespace Unity.Policy
 {
     /// <summary>
     /// A strategy that resolves a value.
     /// </summary>
-    public interface IResolver_Policy : IBuilderPolicy
+    public interface IResolverPolicy : IBuilderPolicy
     {
         /// <summary>
         /// GetOrDefault the value
         /// </summary>
         /// <returns>The value for the dependency.</returns>
-        object Resolve();
+        object Resolve(params ResolverOverride[] args);
     }
 }
