312f014bde275fb7afc87e289bfae956046492b0
Eugene Sadovoi <evgeni@eniks.com>
2017-11-26 17:40:12 -0500

    Refactoring

--- src/Lifetime/LifetimeManagerFactory.cs
+++ src/Lifetime/LifetimeManagerFactory.cs
@@ -33,7 +33,10 @@ namespace Unity.Lifetime
         /// <returns>The new instance.</returns>
         public ILifetimePolicy CreateLifetimePolicy()
         {
-            var lifetime = (LifetimeManager)_containerContext.Container.Resolve(LifetimeType, null);
+            var lifetime = typeof(TransientLifetimeManager) == LifetimeType 
+                         ? new TransientLifetimeManager() 
+                         : (LifetimeManager)_containerContext.Container.Resolve(LifetimeType, null);
+
             if (lifetime is IDisposable)
             {
                 _containerContext.Lifetime.Add(lifetime);
