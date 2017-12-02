87f37b2705e124b212fa7e57abdff52d68131bb5
Eugene Sadovoi <evgeni@eniks.com>
2017-12-02 13:26:30 -0500

    Added IPolicyRegistry.cs

---
+++ src/Container/IPolicyRegistry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Unity.Policy;
+
+namespace Unity.Container
+{
+    public interface IPolicyRegistry
+    {
+        IBuilderPolicy this[Type type, string name, Type policy] { get; set; }
+    }
+}
