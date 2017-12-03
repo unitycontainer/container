27661e56b25e58d1a9a4f3209fc5ee27eaeb41f3
Eugene Sadovoi <evgeni@eniks.com>
2017-12-02 21:07:43 -0500

    IPolicyRegistry

--- src/Container/IPolicyRegistry.cs
+++ src/Container/IPolicyRegistry.cs
@@ -7,4 +7,18 @@ namespace Unity.Container
     {
         IBuilderPolicy this[Type type, string name, Type policy] { get; set; }
     }
+
+
+    public static class PolicyRegistryExtensions
+    {
+        public static T Get<T>(this IPolicyRegistry registry, Type type, string name)
+        {
+            return (T) registry[type, name, typeof(T)];
+        }
+
+        public static void Set<T>(this IPolicyRegistry registry, Type type, string name, IBuilderPolicy policy)
+        {
+            registry[type, name, typeof(T)] = policy;
+        }
+    }
 }
