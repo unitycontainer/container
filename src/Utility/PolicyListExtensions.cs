9abb4aef5cdd698b543cb48d08f6c81cc7a902cd
Eugene Sadovoi <evgeni@eniks.com>
2017-12-14 11:32:48 -0500

    Update PolicyListExtensions.cs

--- src/Utility/PolicyListExtensions.cs
+++ src/Utility/PolicyListExtensions.cs
@@ -14,6 +14,38 @@ namespace Unity.Policy
     public static class PolicyListExtensions
     {
         /// <summary>
+        /// GetOrDefault the non default policy.
+        /// </summary>
+        /// <param name="policyInterface">The interface the policy is registered under.</param>
+        /// <param name="buildKey">The key the policy applies to.</param>
+        /// <param name="containingPolicyList">The policy list in the chain that the searched for policy was found in, null if the policy was
+        /// not found.</param>
+        /// <returns>The policy in the list if present; returns null otherwise.</returns>
+        public static IBuilderPolicy Get(this IPolicyList policies, Type policyInterface, object buildKey, out IPolicyList containingPolicyList)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets an individual policy.
+        /// </summary>
+        /// <param name="policyInterface">The <see cref="Type"/> of the policy.</param>
+        /// <param name="policy">The policy to be registered.</param>
+        /// <param name="buildKey">The key the policy applies.</param>
+        public static void Set(this IPolicyList policies, Type policyInterface, IBuilderPolicy policy, object buildKey = null)
+        {
+        }
+
+        /// <summary>
+        /// Removes an individual policy type for a build key.
+        /// </summary>
+        /// <param name="policyInterface">The type of policy to remove.</param>
+        /// <param name="buildKey">The key the policy applies.</param>
+        public static void Clear(this IPolicyList policies, Type policyInterface, object buildKey)
+        {
+        }
+        
+        /// <summary>
         /// Removes an individual policy type for a build key.
         /// </summary>
         /// <typeparam name="TPolicyInterface">The type the policy was registered as.</typeparam>
