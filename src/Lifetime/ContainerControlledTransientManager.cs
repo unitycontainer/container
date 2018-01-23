97a2e2979a2dbb54c1429342bd71d0713212a340
Eugene Sadovoi <evgeni@eniks.com>
2018-01-23 14:28:43 -0500

    Fixed #37

---
+++ src/Lifetime/ContainerControlledTransientManager.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Unity.Lifetime
+{
+    /// <summary>
+    /// A special lifetime manager which works like <see cref="TransienLifetimeManager"/>,
+    /// except container remembers all Disposable objects it created. Once container
+    /// is disposed all these objects are disposed as well.
+    /// </summary>
+    public class ContainerControlledTransientManager : LifetimeManager
+    {
+        public override void SetValue(object newValue, ILifetimeContainer container = null)
+        {
+            if (newValue is IDisposable disposable)
+                container?.Add(disposable);
+        }
+
+        public override object GetValue(ILifetimeContainer container = null)
+        {
+            return null;
+        }
+
+        public override void RemoveValue(ILifetimeContainer container = null)
+        {
+        }
+
+        protected override LifetimeManager OnCreateLifetimeManager()
+        {
+            return this;
+        }
+
+        public override bool InUse { get => false; set => base.InUse = false; }
+    }
+}
