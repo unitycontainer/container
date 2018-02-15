c7ae07650a61cb04954c4b469432048c3385f1cf
Hailong Mu <hailmu@microsoft.com>
2018-02-15 14:00:39 -0800

    Make Lifetime container dispose logic more robust

--- tests/Unity.Tests/ObjectBuilder/LifetimeContainerTest.cs
+++ tests/Unity.Tests/ObjectBuilder/LifetimeContainerTest.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
 
 using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Unity;
 using Unity.Container.Lifetime;
 using Unity.Lifetime;
@@ -120,6 +122,104 @@ namespace Microsoft.Practices.ObjectBuilder2.Tests
             ILifetimeContainer container = new LifetimeContainer();
 
             container.Remove(new object());
+        }
+
+        [TestMethod]
+        public void ShouldDisposeAsManyAsPossibleWhenTaskExeptionIsThrown()
+        {
+            var obj1 = new Mock<IDisposable>();
+            var obj3 = new Mock<IDisposable>();
+
+            try
+            {
+                using (var container = new UnityContainer())
+                {
+                    container.RegisterInstance(nameof(obj1), obj1.Object);
+                    var obj2 = Task.Run(async () => await Task.Delay(10000));
+                    container.RegisterInstance(nameof(obj2), obj2);
+                    container.RegisterInstance(nameof(obj3), obj3.Object);
+                }
+
+                Assert.Fail("Exceptions should be thrown");
+            }
+            catch (InvalidOperationException e) when(e.Message.Contains("A task may only be disposed if it is in a completion state"))
+            {
+            }
+
+            obj1.Verify(_ => _.Dispose());
+            obj3.Verify(_ => _.Dispose());
+        }
+
+        [TestMethod]
+        public void ShouldDisposeAsManyAsPossibleWhenSingleExeptionIsThrown()
+        {
+            var obj1 = new Mock<IDisposable>();
+            var obj2 = new Mock<IDisposable>();
+            obj2.Setup(_ => _.Dispose()).Callback(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            var obj3 = new Mock<IDisposable>();
+
+            try
+            {
+                using (var container = new UnityContainer())
+                {
+                    container.RegisterInstance(nameof(obj1), obj1.Object);
+                    container.RegisterInstance(nameof(obj2), obj2.Object);
+                    container.RegisterInstance(nameof(obj3), obj3.Object);
+                }
+
+                Assert.Fail("Exceptions should be thrown");
+            }
+            catch (NotImplementedException)
+            {
+            }
+
+            obj1.Verify(_ => _.Dispose());
+            obj2.Verify(_ => _.Dispose());
+            obj3.Verify(_ => _.Dispose());
+        }
+
+        [TestMethod]
+        public void ShouldDisposeAsManyAsPossibleWhenExeptionsAreThrown()
+        {
+            var obj1 = new Mock<IDisposable>();
+            var obj2 = new Mock<IDisposable>();
+            obj2.Setup(_ => _.Dispose()).Callback(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            var obj3 = new Mock<IDisposable>();
+            var obj4 = new Mock<IDisposable>();
+            obj4.Setup(_ => _.Dispose()).Callback(() =>
+            {
+                throw new NotImplementedException();
+            });
+
+            try
+            {
+                using (var container = new UnityContainer())
+                {
+                    container.RegisterInstance(nameof(obj1), obj1.Object);
+                    container.RegisterInstance(nameof(obj2), obj2.Object);
+                    container.RegisterInstance(nameof(obj3), obj3.Object);
+                    container.RegisterInstance(nameof(obj4), obj4.Object);
+                }
+
+                Assert.Fail("Exceptions should be thrown");
+            }
+            catch (AggregateException e)
+            {
+                Assert.AreEqual(2, e.InnerExceptions.Count);
+            }
+
+            obj1.Verify(_ => _.Dispose());
+            obj2.Verify(_ => _.Dispose());
+            obj3.Verify(_ => _.Dispose());
+            obj4.Verify(_ => _.Dispose());
         }
 
         private class DisposableObject : IDisposable
