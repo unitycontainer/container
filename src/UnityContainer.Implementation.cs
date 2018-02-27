90ff63cbe8886d5aa6f217c85c671603dfea4439
Eugene Sadovoi <evgeni@eniks.com>
2018-02-27 10:59:24 -0500

    Update UnityContainer.Implementation.cs

--- src/UnityContainer.Implementation.cs
+++ src/UnityContainer.Implementation.cs
@@ -111,8 +111,8 @@ namespace Unity
             _lifetimeContainer.Add(_buildPlanStrategies);
 
             // Main strategy chain
-            _strategies.Add(new ArrayResolveStrategy(), UnityBuildStage.Enumerable);
-            _strategies.Add(new EnumerableStrategy(), UnityBuildStage.Enumerable);
+            _strategies.Add(new ArrayResolveStrategy(typeof(UnityContainer).GetTypeInfo().GetDeclaredMethod(nameof(ResolveArray))), UnityBuildStage.Enumerable);
+            _strategies.Add(new EnumerableResolveStrategy(typeof(UnityContainer).GetTypeInfo().GetDeclaredMethod(nameof(ResolveEnumerable))), UnityBuildStage.Enumerable);
             _strategies.Add(new BuildKeyMappingStrategy(), UnityBuildStage.TypeMapping);
             _strategies.Add(new LifetimeStrategy(), UnityBuildStage.Lifetime);
             _strategies.Add(new BuildPlanStrategy(), UnityBuildStage.Creation);
