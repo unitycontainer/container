162e0e78d41d042f3f921606b167b165aae74fa2
Eugene Sadovoi <evgeni@eniks.com>
2018-03-30 19:37:49 -0400

    Fixed unitycontainer/unity#212

--- src/UnityContainer.ServiceProvider.cs
+++ src/UnityContainer.ServiceProvider.cs
@@ -4,13 +4,13 @@ using Unity.Registration;
 
 namespace Unity
 {
-    public partial class UnityContainer : IServiceProvider
-    {
-        public object GetService(Type serviceType)
-        {
-            var registration = GetRegistration(serviceType ?? throw new ArgumentNullException(nameof(serviceType)), null);
-            var context = new BuilderContext(this, (InternalRegistration)registration, null, null);
-            return NotThrowingBuildUp(context);
-        }
-    }
+    //public partial class UnityContainer : IServiceProvider
+    //{
+    //    public object GetService(Type serviceType)
+    //    {
+    //        var registration = GetRegistration(serviceType ?? throw new ArgumentNullException(nameof(serviceType)), null);
+    //        var context = new BuilderContext(this, (InternalRegistration)registration, null, null);
+    //        return NotThrowingBuildUp(context);
+    //    }
+    //}
 }
