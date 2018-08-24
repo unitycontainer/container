77d261b626370adeab3f224c6bb000e9bb21f986
Eugene Sadovoi <evgeni@eniks.com>
2018-08-24 12:24:23 -0400

    Update EnumerableResolveStrategy.cs

--- src/Strategies/EnumerableResolveStrategy.cs
+++ src/Strategies/EnumerableResolveStrategy.cs
@@ -80,7 +80,7 @@ namespace Unity.Strategies
                     return false;
             }
 
-             return namedType is InternalRegistration registration &&
+             return namedType is InternalRegistration registration && null != registration.Type &&
                     registration.Type.GetTypeInfo().IsGenericType && 
                     typeof(IEnumerable<>) == registration.Type.GetGenericTypeDefinition();
         }
