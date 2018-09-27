04b688d0bf2a2f05588192f9bbbbbe047fc9a388
Eugene Sadovoi <evgeni@eniks.com>
2018-09-27 19:44:34 -0400

    Moved Events and Extensions into Container

--- src/Utility/UnityContainerExtensions.cs
+++ src/Utility/UnityContainerExtensions.cs
@@ -1,9 +1,6 @@
-﻿
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Extension;
 using Unity.Lifetime;
 using Unity.Registration;
 using Unity.Resolution;
@@ -750,40 +747,6 @@ namespace Unity
         public static object BuildUp(this IUnityContainer container, Type t, object existing, params ResolverOverride[] resolverOverrides)
         {
             return (container ?? throw new ArgumentNullException(nameof(container))).BuildUp(t, existing, null, resolverOverrides);
-        }
-
-        #endregion
-
-        #region Extension management and configuration
-
-        /// <summary>
-        /// Creates a new extension object and adds it to the container.
-        /// </summary>
-        /// <typeparam name="TExtension">Type of <see cref="UnityContainerExtension"/> to add. The extension type
-        /// will be resolved from within the supplied <paramref name="container"/>.</typeparam>
-        /// <param name="container">Container to add the extension to.</param>
-        /// <returns>The <see cref="Unity.IUnityContainer"/> object that this method was called on (this in C#, Me in Visual Basic).</returns>
-        public static IUnityContainer AddNewExtension<TExtension>(this IUnityContainer container)
-            where TExtension : UnityContainerExtension
-        {
-            TExtension newExtension = (container ?? throw new ArgumentNullException(nameof(container))).Resolve<TExtension>();
-            return container.AddExtension(newExtension);
-        }
-
-        /// <summary>
-        /// Resolve access to a configuration interface exposed by an extension.
-        /// </summary>
-        /// <remarks>Extensions can expose configuration interfaces as well as adding
-        /// strategies and policies to the container. This method walks the list of
-        /// added extensions and returns the first one that implements the requested type.
-        /// </remarks>
-        /// <typeparam name="TConfigurator">The configuration interface required.</typeparam>
-        /// <param name="container">Container to configure.</param>
-        /// <returns>The requested extension's configuration interface, or null if not found.</returns>
-        public static TConfigurator Configure<TConfigurator>(this IUnityContainer container)
-            where TConfigurator : IUnityContainerExtensionConfigurator
-        {
-            return (TConfigurator)(container ?? throw new ArgumentNullException(nameof(container))).Configure(typeof(TConfigurator));
         }
 
         #endregion
