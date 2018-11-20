e74262e6cc984a07cfac737f49f01301a0c4f154
Traian Zaprianov <Traian.Zaprianov@docuware.com>
2018-11-20 11:10:12 +0200

    Add benchmark tests which measures resolve and resolve in child container

---
+++ tests/Performance/Tests/ChildContainer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Runner.Tests;
+using Unity;
+using Unity.Lifetime;
+
+namespace Performance.Tests
+{
+    [BenchmarkCategory("ChildContainer")]
+    [MemoryDiagnoser]
+    public class ChildContainer
+    {
+        private IUnityContainer _container;
+
+        [Params(1, 2, 3, 4, 10, 50, 100)] public int Resolves;
+
+        [GlobalSetup]
+        public void SetupContainer()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<Poco>();
+            _container.RegisterType<IService, Service>();
+            _container.RegisterType<IService, Service>("1");
+            _container.RegisterType<IService, Service>("2");
+
+            for (var i = 0; i < 100; i++)
+                _container.RegisterType<DisposableObject>(i.ToString(), new HierarchicalLifetimeManager());
+        }
+
+        [Benchmark(Baseline = true)]
+        public void CreateChildContainer()
+        {
+            using (_container.CreateChildContainer())
+            {
+            }
+        }
+
+        //[Benchmark]
+        //public void ChildTransient()
+        //{
+        //    using (var child = _container.CreateChildContainer())
+        //    {
+        //        for (var i = 0; i < Resolves; i++)
+        //            child.Resolve(typeof(Poco), null);
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void ChildMapping()
+        //{
+        //    using (var child = _container.CreateChildContainer())
+        //    {
+        //        for (var i = 0; i < Resolves; i++)
+        //            child.Resolve(typeof(IService), null);
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void ChildArray()
+        //{
+        //    using (var child = _container.CreateChildContainer())
+        //    {
+        //        for (var i = 0; i < Resolves; i++)
+        //            child.Resolve(typeof(IService[]), null);
+        //    }
+        //}
+
+        //[Benchmark]
+        //public void ChildEnumerable()
+        //{
+        //    using (var child = _container.CreateChildContainer())
+        //    {
+        //        for (var i = 0; i < Resolves; i++)
+        //            child.Resolve(typeof(IEnumerable<IService>), null);
+        //    }
+        //}
+
+        [Benchmark]
+        public void ChildDisposableObject()
+        {
+            using (var child = _container.CreateChildContainer())
+            {
+                for (var i = 0; i < Resolves; i++)
+                    child.Resolve<DisposableObject>(i.ToString());
+            }
+        }
+
+
+        private class DisposableObject : IDisposable
+        {
+            public bool WasDisposed = false;
+
+            public virtual void Dispose()
+            {
+                WasDisposed = true;
+            }
+        }
+    }
+}
---
+++ tests/Performance/Tests/Resolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Runner.Tests;
+using Unity;
+
+namespace Performance.Tests
+{
+    [BenchmarkCategory("Resolution")]
+    [MemoryDiagnoser]
+    public class Resolution
+    {
+        private IUnityContainer _container;
+
+        [GlobalSetup]
+        public void SetupContainer()
+        {
+            _container = new UnityContainer();
+            _container.RegisterType<Poco>();
+            _container.RegisterType<IService, Service>();
+            _container.RegisterType<IService, Service>("1");
+            _container.RegisterType<IService, Service>("2");
+        }
+
+        [Benchmark(Baseline = true)]
+        public object Container() => _container.Resolve(typeof(IUnityContainer), null);
+
+        [Benchmark]
+        public object Unregistered() => _container.Resolve(typeof(object), null);
+
+        [Benchmark]
+        public object Transient() => _container.Resolve(typeof(Poco), null);
+
+        [Benchmark]
+        public object Mapping() => _container.Resolve(typeof(IService), null);
+
+        [Benchmark]
+        public object Array() => _container.Resolve(typeof(IService[]), null);
+
+        [Benchmark]
+        public object Enumerable() => _container.Resolve(typeof(IEnumerable<IService>), null);
+    }
+}
