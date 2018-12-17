c3006db8679e987e83e244df951f37167a418753
Eugene Sadovoi <evgeni@eniks.com>
2018-12-17 00:01:19 -0500

    Performance optimization for expression factories

--- src/Policy/IResolverFactory.cs
+++ src/Policy/IResolverFactory.cs
@@ -3,6 +3,10 @@ using Unity.Resolution;
 
 namespace Unity.Policy
 {
+    public delegate ResolveDelegate<TContext> ResolverFactory<TContext>(Type type) 
+        where TContext : IResolveContext;
+
+
     public interface IResolverFactory
     {
         ResolveDelegate<TContext> GetResolver<TContext>(Type type)
