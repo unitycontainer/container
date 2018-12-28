937eba8634df03ccdc9ca90a626db264fce84dba
Eugene Sadovoi <evgeni@eniks.com>
2018-12-28 11:08:32 -0500

    Refactoring

--- src/Resolution/IResolveContextExpression.cs
+++ src/Resolution/IResolveContextExpression.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using System.Reflection;
 using Unity.Policy;
 
@@ -9,26 +7,6 @@ namespace Unity.Resolution
     public class IResolveContextExpression<TContext>
         where TContext : IResolveContext
     {
-        #region Fields
-
-        private static readonly MethodInfo _get =
-            typeof(IPolicyList).GetTypeInfo()
-                .GetDeclaredMethods(nameof(IPolicyList.Get))
-                .First();
-
-        private static readonly MethodInfo _set =
-            typeof(IPolicyList).GetTypeInfo()
-                .GetDeclaredMethods(nameof(IPolicyList.Set))
-                .First();
-
-        private static readonly MethodInfo _resolve =
-            typeof(IResolveContext).GetTypeInfo()
-                .GetDeclaredMethods(nameof(IResolveContext.Resolve))
-                .First(m => 2 == m.GetParameters().Length);
-
-        #endregion
-
-
         #region Constructor
 
         static IResolveContextExpression()
@@ -41,9 +19,9 @@ namespace Unity.Resolution
                                                  .GetParameters()[0]
                                                  .ParameterType;
 
-            Context = Expression.Parameter(contextRefType, "context");
-            Type = Expression.MakeMemberAccess(Context, typeInfo.GetDeclaredProperty(nameof(IResolveContext.Type)));
-            Name = Expression.MakeMemberAccess(Context, typeInfo.GetDeclaredProperty(nameof(IResolveContext.Name)));
+            Context   = Expression.Parameter(contextRefType, "context");
+            Type      = Expression.MakeMemberAccess(Context, typeInfo.GetDeclaredProperty(nameof(IResolveContext.Type)));
+            Name      = Expression.MakeMemberAccess(Context, typeInfo.GetDeclaredProperty(nameof(IResolveContext.Name)));
             Container = Expression.MakeMemberAccess(Context, typeInfo.GetDeclaredProperty(nameof(IResolveContext.Container)));
         }
 
@@ -59,37 +37,6 @@ namespace Unity.Resolution
         public static readonly MemberExpression Type;
 
         public static readonly MemberExpression Name;
-
-        #endregion
-
-
-        #region Methods
-
-        public static Expression Get(Type type, string name, Type policyType)
-        {
-            return Expression.Call(Context, _get,
-                    Expression.Constant(type, typeof(Type)),
-                    Expression.Constant(name, typeof(string)),
-                    Expression.Constant(policyType, typeof(Type)));
-        }
-
-        public static Expression Set(Type type, string name, Type policyType, Expression policy)
-        {
-            return Expression.Call(Context, _set,
-                    Expression.Constant(type, typeof(Type)),
-                    Expression.Constant(name, typeof(string)),
-                    Expression.Constant(policyType, typeof(Type)),
-                    policy);
-        }
-
-        public static Expression Resolve(Type type, string name)
-        {
-            return Expression.Convert(
-                Expression.Call(Context, _resolve,
-                    Expression.Constant(type, typeof(Type)),
-                    Expression.Constant(name, typeof(string))),
-                type);
-        }
 
         #endregion
     }
