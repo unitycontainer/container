82d459ea1d58e69037aeef0f82c71cc194a500d7
Eugene Sadovoi <evgeni@eniks.com>
2019-01-12 13:03:09 -0500

    Closed #126

---
+++ tests/Unity.Diagnostic/Issues.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+using Unity.Builder;
+using Unity.Extension;
+
+namespace GitHub
+{
+    [TestClass]
+    public class Container : Unity.Specification.Diagnostic.Issues.Container.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer(UnityContainer.BuildStrategy.Compiled)
+                .AddExtension(new Diagnostic())
+                .AddExtension(new SpyExtension(new SpyStrategy(), UnityBuildStage.Initialization));
+        }
+    }
+
+}
