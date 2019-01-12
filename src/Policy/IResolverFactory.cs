c75342bf768edc3c78f7424216e888ac932fb936
Eugene Sadovoi <evgeni@eniks.com>
2019-01-12 13:02:48 -0500

    Cleanup

--- src/Policy/IResolverFactory.cs
+++ src/Policy/IResolverFactory.cs
@@ -6,7 +6,7 @@ namespace Unity.Policy
     public delegate ResolveDelegate<TContext> ResolverFactory<TContext>(Type type) 
         where TContext : IResolveContext;
 
-    public interface IResolverFactory<TMemberInfo>
+    public interface IResolverFactory<in TMemberInfo>
     {
         ResolveDelegate<TContext> GetResolver<TContext>(TMemberInfo info)
             where TContext : IResolveContext;
