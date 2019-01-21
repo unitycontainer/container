8e102f7d24c27135daebef2a2eba54cd031b5faf
Eugene Sadovoi <evgeni@eniks.com>
2019-01-21 18:18:08 -0500

    Trying registration builder

---
+++ tests/Unity.Specification/Registration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace Registration
+{
+    [TestClass]
+    public class Native : Unity.Specification.Registration.Native.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer();
+        }
+    }
+
+    [TestClass]
+    public class Extended : Unity.Specification.Registration.Extended.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer();
+        }
+    }
+
+    [TestClass]
+    public class Syntax : Unity.Specification.Registration.Syntax.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer();
+        }
+    }
+}
