d9f1f8a2c2a117cdf10bd240e1878eda644c6bc2
Eugene Sadovoi <evgeni@eniks.com>
2019-01-24 18:50:23 -0500

    Added EnableDiagnostic()

---
+++ src/Extensions/DiagnosticExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Unity
+{
+    public static class DiagnosticExtensions
+    {
+        [Conditional("DEBUG")]
+        public static void EnableDiagnostic(this UnityContainer container)
+        {
+            if (null == container) throw new ArgumentNullException(nameof(container));
+               
+            container.AddExtension(new Diagnostic());
+        }
+    }
+}
