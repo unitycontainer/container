dfbe05c66ddf0d812aa32414f2fca879c0983ab7
Eugene Sadovoi <evgeni@eniks.com>
2019-05-13 19:58:48 -0400

    Added ILifetimeManagerAsync

---
+++ src/Lifetime/Abstracts/ILifetimeManagerAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Unity.Lifetime
+{
+    /// <summary>
+    /// Base for all async lifetime managers.
+    /// </summary>
+    public interface ILifetimeManagerAsync : IDisposable
+    {
+        Func<ILifetimeContainer, object> GetResult { get; }
+
+        Func<object, ILifetimeContainer, object> SetResult { get; }
+
+        Func<ILifetimeContainer, Task<object>> GetTask { get; }
+
+        Func<Task<object>, ILifetimeContainer, Task<object>> SetTask { get; }
+    }
+}
