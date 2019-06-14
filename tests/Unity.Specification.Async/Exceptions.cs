a5cb537a01203e8c20f23eb80009c00aceaf4ee0
Eugene Sadovoi <evgeni@eniks.com>
2019-06-13 22:10:29 -0400

    Resolved conflicts in namespaces

---
+++ tests/Unity.Specification.Async/Exceptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace Compiled
+{
+
+    [TestClass]
+    public class Exceptions : Unity.Specification.Async.Exceptions.SpecificationTests
+    {
+        public override IUnityContainerAsync GetContainer()
+        {
+            return new UnityContainer(ModeFlags.Compiled);
+        }
+    }
+}
+
+namespace Resolved
+{
+
+    [TestClass]
+    public class Exceptions : Unity.Specification.Async.Exceptions.SpecificationTests
+    {
+        public override IUnityContainerAsync GetContainer()
+        {
+            return new UnityContainer(ModeFlags.Activated);
+        }
+    }
+}
