d1571657ee7e6bfcb59d87243463433f9a153898
Eugene Sadocoi <evgeni@eniks.com>
2019-12-20 12:38:52 -0800

    Implements container#193

--- src/Extension/IExtensionContext.cs
+++ src/Extension/IExtensionContext.cs
@@ -22,13 +22,13 @@ namespace Unity.Extension
         /// The container that this context is associated with.
         /// </summary>
         /// <value>The <see cref="IUnityContainer"/> object.</value>
-        public abstract IUnityContainer Container { get; }
+        IUnityContainer Container { get; }
 
         /// <summary>
         /// The <see cref="ILifetimeContainer"/> that this container uses.
         /// </summary>
         /// <value>The <see cref="ILifetimeContainer"/> is used to manage <see cref="IDisposable"/> objects that the container is managing.</value>
-        public abstract ILifetimeContainer Lifetime { get; }
+        ILifetimeContainer Lifetime { get; }
 
         #endregion
 
@@ -39,14 +39,14 @@ namespace Unity.Extension
         /// The strategies this container uses.
         /// </summary>
         /// <value>The <see cref="IStagedStrategyChain{TStrategyType,TStageEnum}"/> that the container uses to build objects.</value>
-        public abstract IStagedStrategyChain<BuilderStrategy, UnityBuildStage> Strategies { get; }
+        IStagedStrategyChain<BuilderStrategy, UnityBuildStage> Strategies { get; }
 
         /// <summary>
         /// The strategies this container uses to construct build plans.
         /// </summary>
         /// <value>The <see cref="IStagedStrategyChain{TStrategyType,TStageEnum}"/> that this container uses when creating
         /// build plans.</value>
-        public abstract IStagedStrategyChain<MemberProcessor, BuilderStage> BuildPlanStrategies { get; }
+        IStagedStrategyChain<MemberProcessor, BuilderStage> BuildPlanStrategies { get; }
 
         #endregion
 
@@ -57,7 +57,7 @@ namespace Unity.Extension
         /// The policies this container uses.
         /// </summary>
         /// <remarks>The <see cref="IPolicyList"/> the that container uses to build objects.</remarks>
-        public abstract IPolicyList Policies { get; }
+        IPolicyList Policies { get; }
 
         #endregion
 
@@ -69,19 +69,19 @@ namespace Unity.Extension
         /// <see cref="IUnityContainer.RegisterType(Type,Type,string,LifetimeManager, InjectionMember[])"/> 
         /// method, or one of its overloads, is called.
         /// </summary>
-        public abstract event EventHandler<RegisterEventArgs> Registering;
+        event EventHandler<RegisterEventArgs> Registering;
 
         /// <summary>
         /// This event is raised when the <see cref="IUnityContainer.RegisterInstance(Type,string,object,LifetimeManager)"/> method,
         /// or one of its overloads, is called.
         /// </summary>
-        public abstract event EventHandler<RegisterInstanceEventArgs> RegisteringInstance;
+        event EventHandler<RegisterInstanceEventArgs> RegisteringInstance;
 
         /// <summary>
         /// This event is raised when the <see cref="IUnityContainer.CreateChildContainer"/> method is called, providing 
         /// the newly created child container to extensions to act on as they see fit.
         /// </summary>
-        public abstract event EventHandler<ChildContainerCreatedEventArgs> ChildContainerCreated;
+        event EventHandler<ChildContainerCreatedEventArgs> ChildContainerCreated;
 
         #endregion
     }
