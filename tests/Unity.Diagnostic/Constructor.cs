527eab5218528b159fcf92046fb4b4dbda428aab
Eugene Sadocoi <evgeni@eniks.com>
2019-12-31 16:58:33 -0800

    Fixing tests

--- tests/Unity.Diagnostic/Constructor.cs
+++ tests/Unity.Diagnostic/Constructor.cs
@@ -1,21 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Unity;
-using Unity.Extension;
 
 namespace Compiled.Constructor
 {
     [TestClass]
     public class Annotation : Unity.Specification.Diagnostic.Constructor.Annotation.SpecificationTests
-    {
-        public override IUnityContainer GetContainer()
-        {
-            return new UnityContainer().AddExtension(new ForceCompillation())
-                                       .AddExtension(new Diagnostic());
-        }
-    }
-
-    [TestClass]
-    public class Parameters : Unity.Specification.Diagnostic.Constructor.Parameters.SpecificationTests
     {
         public override IUnityContainer GetContainer()
         {
@@ -35,12 +24,41 @@ namespace Compiled.Constructor
     }
 
     [TestClass]
-    public class Injection : Unity.Specification.Diagnostic.Constructor.Injection.SpecificationTests
+    public class Parameters : Unity.Specification.Constructor.Parameters.Validation.SpecificationTests
     {
         public override IUnityContainer GetContainer()
         {
             return new UnityContainer().AddExtension(new ForceCompillation())
                                        .AddExtension(new Diagnostic());
+        }
+    }
+    [TestClass]
+    public class Attribute : Unity.Specification.Constructor.Attribute.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceCompillation());
+        }
+    }
+
+    [TestClass]
+    public class Injection : Unity.Specification.Constructor.Injection.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceCompillation());
+        }
+    }
+
+    [TestClass]
+    public class Overrides : Unity.Specification.Constructor.Overrides.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceCompillation());
         }
     }
 }
@@ -58,16 +76,6 @@ namespace Resolved.Constructor
     }
 
     [TestClass]
-    public class Parameters : Unity.Specification.Diagnostic.Constructor.Parameters.SpecificationTests
-    {
-        public override IUnityContainer GetContainer()
-        {
-            return new UnityContainer().AddExtension(new ForceActivation())
-                                       .AddExtension(new Diagnostic());
-        }
-    }
-
-    [TestClass]
     public class Types : Unity.Specification.Diagnostic.Constructor.Types.SpecificationTests
     {
         public override IUnityContainer GetContainer()
@@ -78,7 +86,7 @@ namespace Resolved.Constructor
     }
 
     [TestClass]
-    public class Injection : Unity.Specification.Diagnostic.Constructor.Injection.SpecificationTests
+    public class Parameters : Unity.Specification.Constructor.Parameters.Validation.SpecificationTests
     {
         public override IUnityContainer GetContainer()
         {
@@ -86,4 +94,35 @@ namespace Resolved.Constructor
                                        .AddExtension(new Diagnostic());
         }
     }
+
+    [TestClass]
+    public class Attribute : Unity.Specification.Constructor.Attribute.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceActivation());
+        }
+    }
+
+    [TestClass]
+    public class Injection : Unity.Specification.Constructor.Injection.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceActivation());
+        }
+    }
+
+    [TestClass]
+    public class Overrides : Unity.Specification.Constructor.Overrides.Validation.SpecificationTests
+    {
+        public override IUnityContainer GetContainer()
+        {
+            return new UnityContainer().AddExtension(new Diagnostic())
+                                       .AddExtension(new ForceActivation());
+        }
+    }
+
 }
