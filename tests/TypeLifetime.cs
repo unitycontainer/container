26feb2a3816827dce97d69ad99544fade5a4d65c
Eugene Sadovoi <evgeni@eniks.com>
2020-05-22 20:25:26 -0700

    Regression (#137)
    chore: Adding Regression Tests

---
+++ tests/TypeLifetime.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Lifetime.Tests
+{
+    [TestClass]
+    public class TypeLifetime
+    {
+
+        [TestMethod]
+        public void unitycontainer_container_88()
+        {
+        }
+
+        [TestMethod]
+        public void unitycontainer_container_92()
+        {
+        }
+
+        [TestMethod]
+        public void unitycontainer_unity_204_1()
+        {
+        }
+
+
+        [TestMethod]
+        public void unitycontainer_unity_204_2()
+        {
+        }
+
+
+        [TestMethod]
+        public void unitycontainer_container_82()
+        {
+        }
+
+    }
+}
