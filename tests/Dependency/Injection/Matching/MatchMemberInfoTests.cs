f4c3b4eb85fb11d28b8f8878d56ff5e74e885ed7
Eugene Sadovoi <evgeni@eniks.com>
2020-06-01 15:21:05 -0700

    test: Adding tests for object matching

--- tests/Dependency/Injection/Matching/MatchMemberInfoTests.cs
+++ tests/Dependency/Injection/Matching/MatchMemberInfoTests.cs
@@ -16,6 +16,14 @@ namespace Injection.Matching
             Assert.AreEqual(result, type.MatchesType(match));
         }
 
+        [DataTestMethod]
+        [DynamicData(nameof(GetMatchesObjectData), DynamicDataSourceType.Method)]
+        public void MatchesObject(object parameter, Type match, bool result)
+        {
+            // Validate
+            Assert.AreEqual(result, parameter.MatchesObject(match));
+        }
+
 
         #region Test Data
 
@@ -40,6 +48,36 @@ namespace Injection.Matching
             yield return new object[] { typeof(List<int>),  typeof(List<>),     false };
             yield return new object[] { typeof(List<>),     typeof(List<int>),  true };
             yield return new object[] { typeof(List<int>),  typeof(List<int>),  true };
+        }
+
+        public static IEnumerable<object[]> GetMatchesObjectData()
+        {
+            // Generic
+
+            // Array
+   // TODO: yield return new object[] { new string[0],      typeof(int[]),      false };
+            yield return new object[] { new string[0],      typeof(int[]),      true };
+
+            // match.IsAssignableFrom
+            yield return new object[] { new object[0],      typeof(object),     true };
+            yield return new object[] { new object[0],      typeof(Array),      true };
+            yield return new object[] { new object[0],      typeof(object[]),   true };
+            yield return new object[] { new object[0],      typeof(string[]),   true };
+            yield return new object[] { new string[0],      typeof(object),     true };
+            yield return new object[] { new string[0],      typeof(Array),      true };
+            yield return new object[] { new string[0],      typeof(object[]),   true };
+            yield return new object[] { new string[0],      typeof(string[]),   true };
+            yield return new object[] { new string[0],      typeof(string),     false };
+
+            yield return new object[] { typeof(object),     typeof(object),     true };
+            yield return new object[] { new object(),       typeof(object),     true };
+
+            yield return new object[] { null,               null,               true };
+            yield return new object[] { null,               typeof(object),     true };
+
+            yield return new object[] { string.Empty,       typeof(string),     true };
+            yield return new object[] { string.Empty,       typeof(object),     true };
+            yield return new object[] { string.Empty,       typeof(int),        false };
         }
 
         #endregion
