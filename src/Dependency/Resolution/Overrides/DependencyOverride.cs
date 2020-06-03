a085539013bd8cb5c2f9cc4941aef8cd2e36acda
Eugene Sadovoi <evgeni@eniks.com>
2020-06-03 09:33:02 -0700

    Added additional constructor to DependencyOverride
    Fixed #158

--- src/Dependency/Resolution/Overrides/DependencyOverride.cs
+++ src/Dependency/Resolution/Overrides/DependencyOverride.cs
@@ -8,12 +8,11 @@ namespace Unity.Resolution
     /// given type, regardless of where it appears in the object graph.
     /// </summary>
     public class DependencyOverride : ResolverOverride, 
-                                      IEquatable<NamedType>,
-                                      IResolve
+                                      IEquatable<NamedType>
     {
         #region Fields
 
-        protected readonly object Value;
+        protected readonly Type Type;
 
         #endregion
 
@@ -27,9 +26,9 @@ namespace Unity.Resolution
         /// <param name="type">Type of the dependency.</param>
         /// <param name="value">Value to override with</param>
         public DependencyOverride(Type type, object value)
-            : base(null, type, null)
+            : base(null, value)
         {
-            Value = value;
+            Type = type;
         }
 
         /// <summary>
@@ -39,9 +38,8 @@ namespace Unity.Resolution
         /// <param name="name">Name of the dependency</param>
         /// <param name="value">Value to override with</param>
         public DependencyOverride(string name, object value)
-            : base(null, null, name)
+            : base(name, value)
         {
-            Value = value;
         }
 
 
@@ -53,23 +51,23 @@ namespace Unity.Resolution
         /// <param name="type">Type of the dependency.</param>
         /// <param name="value">Value to override with</param>
         public DependencyOverride(Type type, string name, object value)
-            : base(null, type, name)
+            : base(name, value)
         {
-            Value = value;
+            Type = type;
         }
 
         /// <summary>
         /// Create an instance of <see cref="DependencyOverride"/> to override
         /// dependency on cpecific type matching the given type and a name
         /// </summary>
-        /// <param name="target">Target type</param>
+        /// <param name="target">Target type to override dependency on</param>
         /// <param name="name">Name of the dependency</param>
         /// <param name="type">Type of the dependency.</param>
         /// <param name="value">Value to override with</param>
         public DependencyOverride(Type target, Type type, string name, object value)
-            : base(target, type, name)
+            : base(target, name, value)
         {
-            Value = value;
+            Type = type;
         }
 
         #endregion
@@ -105,26 +103,6 @@ namespace Unity.Resolution
         }
 
         #endregion
-
-
-        #region IResolverPolicy
-
-        public object Resolve<TContext>(ref TContext context) 
-            where TContext : IResolveContext
-        {
-            if (Value is IResolve policy)
-                return policy.Resolve(ref context);
-
-            if (Value is IResolverFactory<Type> factory)
-            {
-                var resolveDelegate = factory.GetResolver<TContext>(Type);
-                return resolveDelegate(ref context);
-            }
-
-            return Value;
-        }
-
-        #endregion
     }
 
     /// <summary>
@@ -134,6 +112,24 @@ namespace Unity.Resolution
     /// <typeparam name="T">Type of the dependency to override.</typeparam>
     public class DependencyOverride<T> : DependencyOverride
     {
+        /// <summary>
+        /// Create an instance of <see cref="DependencyOverride"/> to override
+        /// dependencies matching the given type and a name
+        /// </summary>
+        /// <remarks>
+        /// This constructor creates an override that will match with any
+        /// target type as long as the dependency type and name match. To 
+        /// target specific type use <see cref="ResolverOverride.OnType(Type)"/> 
+        /// method.
+        /// </remarks>
+        /// <param name="target">Target type to override dependency on</param>
+        /// <param name="name">Name of the dependency</param>
+        /// <param name="value">Override value</param>
+        public DependencyOverride(Type target, string name, object value)
+            : base(target, typeof(T), name, value)
+        {
+        }
+
         /// <summary>
         /// Create an instance of <see cref="DependencyOverride"/> to override
         /// dependencies matching the given type and a name
