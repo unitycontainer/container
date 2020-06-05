898e6cd93068fcd87e0cf37ed03b9764e43b218a
Eugene Sadovoi <evgeni@eniks.com>
2020-06-05 15:29:39 -0700

    test: adding tests for NamedType

---
+++ tests/Dependency/Resolution/NamedTypeTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Unity.Resolution;
+
+namespace Resolution.Basics
+{
+    [TestClass]
+    public class NamedTypeTests
+    {
+        private static Type  TestType = typeof(NamedTypeTests);
+        private const string TestName = "98a884c0-7eae-4926-8f3e-bf1f42a05be6";
+
+        [TestMethod]
+        public void HashCodeNotZeroTest()
+        {
+            // Arrange
+            NamedType structure = new NamedType();
+
+            // Validate
+            Assert.AreNotEqual(0, structure.GetHashCode());
+        }
+
+        [TestMethod]
+        public void HashCodeRepeatableTest()
+        {
+            // Arrange
+            var typeHash = TestType.GetHashCode();
+            var nameHash = TestName.GetHashCode();
+
+            NamedType structure1 = new NamedType { Type = TestType, Name = TestName };
+            NamedType structure2 = new NamedType { Type = typeof(NamedTypeTests), Name = "98a884c0-7eae-4926-8f3e-bf1f42a05be6" };
+
+            // Validate
+            Assert.AreEqual(structure1.GetHashCode(), structure2.GetHashCode());
+            Assert.AreEqual(structure1.GetHashCode(), NamedType.GetHashCode(typeHash, nameHash));
+            Assert.AreEqual(NamedType.GetHashCode(typeHash, nameHash), NamedType.GetHashCode(typeHash, nameHash));
+        }
+
+
+        [TestMethod]
+        public void EqualsTest()
+        {
+            // Arrange
+            NamedType structure1 = new NamedType { Type = TestType, Name = TestName };
+            NamedType structure2 = new NamedType { Type = typeof(NamedTypeTests), Name = "98a884c0-7eae-4926-8f3e-bf1f42a05be6" };
+            NamedType structure3 = new NamedType { Type = typeof(NamedTypeTests) };
+            NamedType structure4 = new NamedType { Name = TestName };
+
+            // Validate
+            Assert.IsTrue( structure1.Equals(structure2));
+            Assert.IsFalse(structure1.Equals(structure3));
+            Assert.IsFalse(structure1.Equals(structure4));
+
+            Assert.IsTrue(structure1 == structure2);
+            Assert.IsTrue(structure2 != structure3);
+            Assert.IsTrue(structure3 != structure4);
+
+            Assert.IsFalse(structure1 != structure2);
+            Assert.IsFalse(structure2 == structure3);
+            Assert.IsFalse(structure3 == structure4);
+        }
+
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            // Arrange
+            NamedType structure = new NamedType { Type = TestType, Name = TestName };
+            var data = structure.ToString();
+
+            // Validate
+            Assert.IsTrue(data.Contains(TestName));
+            Assert.IsTrue(data.Contains(TestType.Name));
+        }
+
+        [TestMethod]
+        public void ToStringCold()
+        {
+            // Arrange
+            NamedType structure = new NamedType();
+            var data = structure.ToString();
+
+            // Validate
+            Assert.IsFalse(data.Contains(TestName));
+            Assert.IsFalse(data.Contains(TestType.Name));
+        }
+    }
+}
--- tests/Dependency/Resolution/ParamCollectionTests.cs
+++ tests/Dependency/Resolution/ParamCollectionTests.cs
@@ -5,11 +5,11 @@ using Unity.Resolution;
 namespace Resolution.Overrides
 {
     [TestClass]
-    public class ParameterOverridesTests
+    public class ParamCollectionTests
     {
         private const string Name = "name";
         private static object TestValue = new object();
-        private static ResolverOverride Target = new ParameterOverride(typeof(string), Name, TestValue).OnType(typeof(ParameterOverridesTests));
+        private static ResolverOverride Target = new ParameterOverride(typeof(string), Name, TestValue).OnType(typeof(ParamCollectionTests));
         private ParameterOverrides _parameters;
 
         [TestInitialize]
@@ -34,7 +34,7 @@ namespace Resolution.Overrides
         [TestMethod]
         public void ParametersOnTypeTest()
         {
-            foreach (var parameter in _parameters.OnType(typeof(ParameterOverridesTests)))
+            foreach (var parameter in _parameters.OnType(typeof(ParamCollectionTests)))
             {
                 Assert.IsInstanceOfType(parameter, typeof(ParameterOverride));
                 Assert.IsTrue(parameter.Equals(Target));
@@ -44,7 +44,7 @@ namespace Resolution.Overrides
         [TestMethod]
         public void ParametersOnTypeGeneric()
         {
-            foreach (var parameter in _parameters.OnType<ParameterOverridesTests>())
+            foreach (var parameter in _parameters.OnType<ParamCollectionTests>())
             {
                 Assert.IsInstanceOfType(parameter, typeof(ParameterOverride));
                 Assert.IsTrue(parameter.Equals(Target));
