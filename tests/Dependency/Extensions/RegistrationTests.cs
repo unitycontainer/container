205b462d3d304447e6f143b2d4a484853d085eed
Eugene Sadovoi <evgeni@eniks.com>
2020-06-06 13:04:04 -0700

    chore: cleanup

--- tests/Dependency/Extensions/RegistrationTests.cs
+++ tests/Dependency/Extensions/RegistrationTests.cs
@@ -22,41 +22,5 @@ namespace Injection.Extensions
             yield return new object[] { Inject.Array(typeof(string), string.Empty) };
             yield return new object[] { Inject.Array<string>(string.Empty) };
         }
-
-        // TODO: Issue 146
-        //[DataTestMethod]
-        //[ExpectedException(typeof(ArgumentNullException))]
-        //[DynamicData(nameof(GetInjectArrayInvalidVariants), DynamicDataSourceType.Method)]
-        //public void InjectArrayValidationTests()
-        //{
-        //    _ = Inject.Array(null);
-        //}
-
-        //public static IEnumerable<object[]> GetInjectArrayInvalidVariants()
-        //{
-        //    // TODO: Issue #146 yield return new object[] { Inject.Array(null) };
-        //    yield return new object[] { Inject.Array(typeof(string), string.Empty) };
-        //}
-
-        //[DataTestMethod]
-        //[DynamicData(nameof(GetInjectParameterVariants), DynamicDataSourceType.Method)]
-        //public void InjectParameterTests(object instance)
-        //{
-        //    // Validate
-        //    Assert.IsInstanceOfType(instance, typeof(ParameterBase));
-        //}
-
-        //public static IEnumerable<object[]> GetInjectParameterVariants()
-        //{
-        //    yield return new object[] { Inject.Parameter(new object()) };
-        //}
-
-        //[TestMethod]
-        //public void InjectFieldTests()
-        //{ }
-
-        //[TestMethod]
-        //public void InjectPropertyTests()
-        //{ }
     }
 }
