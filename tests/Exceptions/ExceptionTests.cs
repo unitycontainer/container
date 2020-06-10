e030c53150f22e9b826d83ca96ae8008e0bcdafd
Eugene Sadovoi <evgeni@eniks.com>
2020-06-10 16:17:38 -0700

    Excluding netcoreapp serialization tests

--- tests/Exceptions/ExceptionTests.cs
+++ tests/Exceptions/ExceptionTests.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Unity;
 
 namespace Exceptions.Tests
 {
     [TestClass]
     public partial class ExceptionTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidationTest()
+        {
+            _ = new ResolutionFailedException(null, null, null);
+        }
     }
 }
