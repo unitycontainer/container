efba449bb2364bf4198a2e4dbb605023650321a4
Eugene Sadovoi <evgeni@eniks.com>
2020-06-30 09:08:45 -0700

    Changing IUnityContainerExtensionConfigurator interface
    Fixed #224

---
+++ src/Container/Extension/IUnityContainerExtensionConfigurator.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Unity.Extension
+{
+    /// <summary>
+    /// Base interface for all extension configurations.
+    /// </summary>
+    public interface IUnityContainerExtensionConfigurator
+    {
+        /// <summary>
+        /// The container that owns configured extension.
+        /// </summary>
+        UnityContainer Container { get; }
+    }
+}
