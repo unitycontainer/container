4ec81b5dc98e51cd46fdd54d99fb0269c05c4883
Eugene Sadovoi <evgeni@eniks.com>
2020-07-29 12:18:20 -0700

    Adding more tests

--- Benchmarks/Container/ContainerAPI.cs
+++ Benchmarks/Container/ContainerAPI.cs
@@ -13,35 +13,47 @@ namespace Unity.Benchmarks
 {
     public partial class ContainerAPI
     {
-        protected IUnityContainer Container = new UnityContainer();
+        protected IUnityContainer Container;
         protected Consumer Consumer = new Consumer();
+
+        [GlobalSetup]
+        public virtual void GlobalSetup() 
+            => Container = new UnityContainer();
+
 
         [Benchmark(Description = "new UnityContainer()")]
         [BenchmarkCategory("new")]
-        public object NewUnityContainer()
-        {
-            return new UnityContainer();
-        }
+        public object NewUnityContainer() 
+            => new UnityContainer();
+
 
         [Benchmark(Description = "CreateChildContainer()")]
         [BenchmarkCategory("child")]
-        public object CreateChildContainer()
-        {
-            return Container.CreateChildContainer(); 
-        }
+        public object CreateChildContainer() 
+            => Container.CreateChildContainer();
+
 
         [Benchmark(Description = "Container.Registrations")]
         [BenchmarkCategory("registrations")]
-        public object Registrations()
-        {
-            return Container.Registrations;
-        }
+        public object Registrations() 
+            => Container.Registrations;
+
 
         [Benchmark(Description = "Registrations.ToArray()")]
         [BenchmarkCategory("registrations", "ToArray")]
-        public void RegistrationsToArray()
-        {
-            Container.Registrations.Consume(Consumer);
-        }
+        public void RegistrationsToArray() 
+            => Container.Registrations.Consume(Consumer);
+
+
+        [Benchmark(Description = "Container.IsRegistered(true)")]
+        [BenchmarkCategory("registration", "true")]
+        public virtual bool IsRegistered() 
+            => Container.IsRegistered(typeof(IUnityContainer), null);
+
+
+        [Benchmark(Description = "Container.IsRegistered(false)")]
+        [BenchmarkCategory("registration", "false")]
+        public virtual bool IsNotRegistered() 
+            => Container.IsRegistered(typeof(object), "name");
     }
 }
