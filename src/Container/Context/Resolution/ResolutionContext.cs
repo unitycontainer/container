a5effe6cc99f7255fe160d3f4b3a12caf4f61373
Eugene Sadovoi <evgeni@eniks.com>
2020-08-28 18:53:33 -0700

    Minor refactoring

---
+++ src/Container/Context/Resolution/ResolutionContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Unity.Container
+{
+    public readonly struct ResolutionContext
+    {
+        #region Fields
+
+        private readonly IntPtr _parent;
+        public readonly string Name;
+
+        #endregion
+
+
+        #region Constructors
+
+        public ResolutionContext(string name)
+        {
+            _parent = IntPtr.Zero;
+            Name = name;
+        }
+
+        public ResolutionContext(ref ResolutionContext parent)
+        {
+            unsafe
+            { 
+                _parent = new IntPtr(Unsafe.AsPointer(ref parent));
+            }
+             
+            Name = Guid.NewGuid().ToString();
+        }
+
+        #endregion
+
+
+        public bool IsChild => _parent != IntPtr.Zero;
+
+        public readonly ref ResolutionContext Parent
+        {
+            get 
+            {
+                unsafe
+                {
+                    return ref Unsafe.AsRef<ResolutionContext>(_parent.ToPointer());
+                }
+            }
+        }
+
+        public long Id => (long)_parent;
+
+    }
+}
