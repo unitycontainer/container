b0c966acb8c2c8cea7232f5eff872386334f70fc
Eugene Sadovoi <evgeni@eniks.com>
2020-09-13 14:03:01 -0700

    Refactoring

---
+++ src/Container/Policies/IDependencyResolverPolicy.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Unity.Policy
+{
+    /// <summary>
+    /// A strategy that is used at build plan execution time
+    /// to resolve a dependent value.
+    /// </summary>
+    public interface IDependencyResolverPolicy
+    {
+        /// <summary>
+        /// Get the value for a dependency.
+        /// </summary>
+        /// <param name="context">Current build context.</param>
+        /// <returns>The value for the dependency.</returns>
+        object Resolve(ref IBuilderContext context);
+    }
+}
