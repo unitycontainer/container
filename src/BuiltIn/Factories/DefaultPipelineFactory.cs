928c36dfc6c8e0b54cfb41e9578833ff4dae6de2
Eugene Sadovoi <evgeni@eniks.com>
2020-09-17 16:06:03 -0700

    Missing peaces

---
+++ src/BuiltIn/Factories/DefaultPipelineFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Unity.Container;
+using Unity.Extension;
+using Unity.Lifetime;
+using Unity.Resolution;
+
+namespace Unity.BuiltIn
+{
+    public static class DefaultPipelineFactory
+    {
+        private static Defaults? _policies;
+
+        public static void Setup(ExtensionContext context)
+        {
+            _policies = (Defaults)context.Policies;
+            _policies.Set(typeof(PipelineFactory), (PipelineFactory)Factory);
+        }
+
+        public static Pipeline Factory(ref ResolutionContext context)
+        {
+            return context.Manager switch
+            {
+                // Transient lifetime
+                LifetimeManager { Style: ResolutionStyle.EveryTime }  => _policies!.OptimizedFactory(ref context),
+                
+                // Once in a while
+                LifetimeManager { Style: ResolutionStyle.OnceInWhile } => _policies!.BalancedFactory(ref context),
+                
+                // Once in a lifetime
+                LifetimeManager { Style: ResolutionStyle.OnceInLifetime } => context.Manager.Category switch
+                {
+                    RegistrationCategory.Type     => _policies!.TypePipeline,
+                    RegistrationCategory.Instance => _policies!.InstancePipeline,
+                    RegistrationCategory.Factory  => _policies!.FactoryPipeline,
+
+                    _ => throw new NotImplementedException(),
+                },
+
+                // Unregistered type
+                null => _policies!.UnregisteredFactory(ref context),
+
+                // Not implemented
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
---
+++ src/Container/Policies/PipelineFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using Unity.Resolution;
+
+namespace Unity.Container
+{
+    public delegate Pipeline PipelineFactory(ref ResolutionContext context);
+}
+
