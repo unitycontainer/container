ee5537df99aaffc3441df5f5806d1d12f4c15904
Eugene Sadovoi <evgeni@eniks.com>
2020-10-28 09:22:11 -0700

    Refactoring

---
+++ src/Container/Unity/Public/Unity.Resolution.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Unity.Container;
+using Unity.Lifetime;
+using Unity.Resolution;
+
+namespace Unity
+{
+    public partial class UnityContainer
+    {
+        /// <inheritdoc />
+//#if NET5_0
+//        [SkipLocalsInit]
+//#endif
+        public object? Resolve(Type type, string? name, params ResolverOverride[] overrides)
+        {
+            var contract = new Contract(type, name);
+            var container = this;
+            bool? isGeneric = null;
+            Contract generic = default;
+            RegistrationCategory cutoff = RegistrationCategory.Uninitialized;
+
+            do
+            {
+                // Look for registration
+                var manager = container._scope.Get(in contract, cutoff);
+                if (null != manager)
+                {
+                    //Registration found, check value
+                    var value = Unsafe.As<LifetimeManager>(manager).GetValue(_scope.Disposables);
+                    if (!ReferenceEquals(RegistrationManager.NoValue, value)) return value;
+
+                    // Resolve from registration
+                    return container.ResolveRegistration(ref contract, manager, overrides);
+                }
+
+                cutoff = RegistrationCategory.Cache;
+
+                // Skip to parent if non generic
+                if (!(isGeneric ??= type.IsGenericType)) continue;
+
+                // Fill the Generic Type Definition
+                if (0 == generic.HashCode) generic = contract.With(type.GetGenericTypeDefinition());
+
+                // Check if generic factory is registered
+                if (null != (manager = container._scope.Get(in contract, in generic)))
+                {
+                    // Build from generic factory
+                    return container.GenericRegistration(ref contract, manager, overrides);
+                }
+            }
+            while (null != (container = container.Parent));
+
+            // No registration found, resolve unregistered
+            return (bool)isGeneric ? ResolveUnregisteredGeneric(ref contract, ref generic, overrides)
+                  : type.IsArray   ? ResolveArray(ref contract, overrides)
+                                   : ResolveUnregistered(ref contract, overrides);
+        }
+        
+
+        /// <inheritdoc />
+        public ValueTask<object?> ResolveAsync(Type type, string? name, params ResolverOverride[] overrides)
+        {
+            var contract = new Contract(type, name);
+            var container = this;
+
+            do
+            {
+                RegistrationManager? manager;
+
+                // Optimistic lookup
+                if (null != (manager = container!._scope.Get(in contract)))
+                {
+                    object? value;
+
+                    // Registration found, check for value
+                    if (RegistrationManager.NoValue != (value = manager.TryGetValue(_scope.Disposables)))
+                        return new ValueTask<object?>(value);
+
+                    // No value, do everything else asynchronously
+                    return new ValueTask<object?>(Task.Factory.StartNew(container.ResolveContractAsync, new RequestInfoAsync(in contract, manager, overrides),
+                        System.Threading.CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default));
+                }
+            }
+            while (null != (container = container.Parent));
+
+            // No registration found, do everything else asynchronously
+            return new ValueTask<object?>(
+                Task.Factory.StartNew(ResolveAsync, new RequestInfoAsync(in contract, overrides),
+                    System.Threading.CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default));
+        }
+    }
+}
