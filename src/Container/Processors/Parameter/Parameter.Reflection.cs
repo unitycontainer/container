b8448ef91bea011c514e4b9281dc424898e30aa7
Eugene Sadovoi <evgeni@eniks.com>
2020-11-18 19:49:09 -0800

    wip: Reflection

--- src/Container/Processors/Parameter/Parameter.Reflection.cs
+++ src/Container/Processors/Parameter/Parameter.Reflection.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Unity.Container
 {
@@ -7,6 +9,13 @@ namespace Unity.Container
     {
         private static ImportType DefaultImportProvider(ref ImportInfo info)
         {
+            info.Attributes = Unsafe.As<Attribute[]>(info.MemberInfo.GetCustomAttributes(false));
+
+            foreach (var _attribute in info.Attributes)
+            { 
+            
+            }
+
             var attribute = info.MemberInfo.GetCustomAttribute<ImportAttribute>(true);
 
             info.Data.ImportType = ImportType.None;
@@ -18,6 +27,40 @@ namespace Unity.Container
             }
             else
             { 
+                info.AllowDefault = false;
+                info.Default.ImportType = ImportType.None;
+            }
+
+            if (null != attribute)
+            {
+                info.Contract = new Contract(attribute.ContractType ?? info.MemberInfo.ParameterType, attribute.ContractName);
+                info.AllowDefault |= attribute.AllowDefault;
+                info.Source = attribute.Source;
+                info.Policy = attribute.RequiredCreationPolicy;
+
+                return ImportType.Attribute;
+            }
+
+            info.Contract = new Contract(info.MemberInfo.ParameterType);
+            info.Source = ImportSource.Any;
+            info.Policy = CreationPolicy.Any;
+
+            return ImportType.None;
+        }
+
+        private static ImportType DefaultImportProviderBackup(ref ImportInfo info)
+        {
+            var attribute = info.MemberInfo.GetCustomAttribute<ImportAttribute>(true);
+
+            info.Data.ImportType = ImportType.None;
+            if (info.MemberInfo.HasDefaultValue)
+            {
+                info.AllowDefault = true;
+                info.Default.Value = info.MemberInfo.DefaultValue;
+                info.Default.ImportType = ImportType.Value;
+            }
+            else
+            {
                 info.AllowDefault = false;
                 info.Default.ImportType = ImportType.None;
             }
