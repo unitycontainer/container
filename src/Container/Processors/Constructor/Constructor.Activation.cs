9fbdd36cb3bd73ba4263929cb24281340e58c552
Eugene Sadovoi <evgeni@eniks.com>
2020-11-19 20:09:03 -0800

    Cleanup

--- src/Container/Processors/Constructor/Constructor.Activation.cs
+++ src/Container/Processors/Constructor/Constructor.Activation.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.Composition;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Unity.Injection;
-using Unity.Lifetime;
 
 namespace Unity.Container
 {
