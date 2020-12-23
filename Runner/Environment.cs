37f4a6a47e8abba84827c1dc343808f661926e58
Eugene Sadovoi <evgeni@eniks.com>
2020-12-22 23:26:15 -0800

    Fixing environment selection

---
+++ Runner/Environment.cs
@@ -0,0 +1,58 @@
+﻿// Version
+
+#if UNITY_V4
+//  Unity v4.0.1
+#endif
+
+#if UNITY_V5
+// Unity v5.11.x
+#endif
+
+#if UNITY_V6
+// Unity v6.x
+#endif
+
+
+// Behavior
+
+#if BEHAVIOR_V4
+// Unity v4.0.1
+#endif
+
+#if BEHAVIOR_V5
+// Unity v5.11.x
+#endif
+
+#if BEHAVIOR_V6
+// v6.x
+#endif
+
+// .NET Framework
+
+#if NET5_0
+// Current Framework NET5.0
+#endif
+#if NET48
+// Current Framework NET4.8
+#endif
+#if NET47
+// Current Framework NET4.7
+#endif
+#if NET471
+// Current Framework NET4.7.1
+#endif
+#if NET472
+// Current Framework NET4.7.2
+#endif
+#if NET46
+// Current Framework NET4.6
+#endif
+#if NET461
+// Current Framework NET4.6.1
+#endif
+#if NET462
+// Current Framework NET4.6.2
+#endif
+#if NET45
+// Current Framework NET4.5
+#endif
