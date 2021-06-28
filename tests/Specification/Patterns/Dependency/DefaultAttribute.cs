ce57f20c729806879517834fc8be0b83b1873b59
Eugene Sadovoi <evgeni@eniks.com>
2021-06-27 17:38:53 -0700

    Splitting test file

--- tests/Specification/Patterns/Dependency/DefaultAttribute.cs
+++ tests/Specification/Patterns/Dependency/DefaultAttribute.cs
@@ -13,34 +13,7 @@ namespace Dependency
     {
         #region With Defaults
 #if !UNITY_V4 // Defaults are not supported in Unity v4
-#if BEHAVIOR_V4
-        [ExpectedException(typeof(ResolutionFailedException))]
-#endif
-        [PatternTestMethod("Dependency with default Value"), TestCategory(CATEGORY_DEPENDENCY)]
-        [DynamicData(nameof(WithDefaultValue_Data))]
-        public virtual void Import_WithDefault_Value(string test, Type type)
-        {
-            // Act
-            var instance = Container.Resolve(type, null) as DependencyBaseType;
 
-            // Validate
-            Assert.IsNotNull(instance);
-            Assert.IsInstanceOfType(instance, type);
-            Assert.AreEqual(instance.Default, instance.Value);
-
-            // Arrange
-            RegisterTypes();
-
-            // Act
-            instance = Container.Resolve(type, null) as DependencyBaseType;
-
-            // Validate
-            Assert.IsNotNull(instance);
-            Assert.IsInstanceOfType(instance, type);
-#if !BEHAVIOR_V5
-            Assert.AreEqual(Container.Resolve(instance.ImportType, null), instance.Value);
-#endif
-        }
 
 #if BEHAVIOR_V4 || BEHAVIOR_V5
         [ExpectedException(typeof(ResolutionFailedException))]
