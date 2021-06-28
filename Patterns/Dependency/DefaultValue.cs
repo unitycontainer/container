a2358ffdd1c1dee6b4fed2783f9ef567363a8e85
Eugene Sadovoi <evgeni@eniks.com>
2021-06-27 17:38:53 -0700

    Splitting test file

---
+++ Patterns/Dependency/DefaultValue.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Regression;
+using System;
+#if UNITY_V4
+using Microsoft.Practices.Unity;
+#else
+using Unity;
+#endif
+
+namespace Dependency
+{
+    public abstract partial class Pattern
+    {
+        #region With Defaults
+
+#if BEHAVIOR_V4
+        [ExpectedException(typeof(ResolutionFailedException))]
+#endif
+        [PatternTestMethod("Dependency with default Value"), TestCategory(CATEGORY_DEPENDENCY)]
+        [DynamicData(nameof(WithDefaultValue_Data))]
+        public virtual void Import_WithDefault_Value(string test, Type type)
+        {
+            // Act
+            var instance = Container.Resolve(type, null) as DependencyBaseType;
+
+            // Validate
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOfType(instance, type);
+            Assert.AreEqual(instance.Default, instance.Value);
+
+            // Arrange
+            RegisterTypes();
+
+            // Act
+            instance = Container.Resolve(type, null) as DependencyBaseType;
+
+            // Validate
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOfType(instance, type);
+#if !BEHAVIOR_V5
+            Assert.AreEqual(Container.Resolve(instance.ImportType, null), instance.Value);
+#endif
+        }
+
+        #endregion
+    }
+}
