e361333ef628dbceaa261d74eb8a3011ad6ca843
ENikS <evgeni@eniks.com>
2023-03-14 11:43:21 -0700

    Fixing PerResolve lifetime in factory registrations

--- src/Container/Strategies/Factory/FactoryStrategy.cs
+++ src/Container/Strategies/Factory/FactoryStrategy.cs
@@ -13,7 +13,12 @@ namespace Unity.Container
                 if (factory is null)
                     context.Error("Invalid Factory");
                 else
-                    context.Existing = factory(context.Container, context.Type, context.Name, context.Overrides);
+                { 
+                    if (context.Registration is Lifetime.PerResolveLifetimeManager)
+                        context.PerResolve = factory(context.Container, context.Type, context.Name, context.Overrides);
+                    else
+                        context.Existing = factory(context.Container, context.Type, context.Name, context.Overrides);
+                }
             }
             catch (Exception ex)
             {
