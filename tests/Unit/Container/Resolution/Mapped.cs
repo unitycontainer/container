67b94712ddf335f1c4cb41ef9855efe04a36245c
ENikS <evgeni@eniks.com>
2023-03-24 08:29:30 -0700

    Adding tests

---
+++ tests/Unit/Container/Resolution/Mapped.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity;
+
+namespace Container
+{
+    public partial class Resolution
+    {
+        [TestMethod, TestProperty(RESOLVE, MAPPED)]
+        public void Resolve_MappedService()
+        {
+            Container.RegisterType<IService, Service>();
+
+            var instance = Container.Resolve<IService>();
+
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOfType(instance, typeof(Service));
+        }
+    }
+}
