50ea2ce999a5ef2b529ede71514f01dbcffb8869
ENikS <evgeni@eniks.com>
2023-03-27 10:23:44 -0700

    Removing unused code

--- src/Extension/Delegates/MatchDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Unity.Extension
-{
-    /// <summary>
-    /// Calculates the match between members
-    /// </summary>
-    public delegate TMatch MatchDelegate<TLeft, TRight, TMatch>(TLeft left, TRight right);
-
-}
