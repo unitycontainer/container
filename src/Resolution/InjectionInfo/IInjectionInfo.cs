89d82629e68883b10ba8fa499464e419f5bfd051
ENikS <evgeni@eniks.com>
2023-04-03 18:27:17 -0700

    Missing files

---
+++ src/Resolution/InjectionInfo/IInjectionInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Unity.Injection;
+
+
+
+
+public interface IInjectionInfo<TMemberInfo> : IInjectionInfo
+{
+    /// <summary>
+    /// One of <see cref="ParameterInfo"/>, <see cref="FieldInfo"/>, or
+    /// <see cref=" PropertyInfo"/>
+    /// </summary>
+    TMemberInfo MemberInfo { get; }
+
+    /// <summary>
+    /// True if annotated with <see cref="Unity.DependencyResolutionAttribute"/>
+    /// </summary>
+    bool IsImport { get; set; }
+}
---
+++ src/Resolution/InjectionInfo/IInjectionInfoProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Unity.Injection;
+
+
+
+public interface IInjectionInfoProvider<TMemberInfo> : IInjectionInfoProvider
+{
+    new void ProvideInfo<TInjectionInfo>(ref TInjectionInfo info)
+        where TInjectionInfo : IInjectionInfo<TMemberInfo>;
+}
