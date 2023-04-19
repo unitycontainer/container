1f9c83e91e2200466c36640e710fd5943e4608a0
ENikS <evgeni@eniks.com>
2023-04-19 12:26:15 -0700

    Adding ILifetimeContainer interface back

---
+++ src/Container/Lifetime/ILifetimeContainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unity.Lifetime
+{
+    /// <summary>
+    /// Interface to a container holding references to all the objects 
+    /// it is responsible to keep alive.
+    /// </summary>
+    public interface ILifetimeContainer
+    {
+        /// <summary>
+        /// Adds an object to the lifetime container.
+        /// </summary>
+        /// <param name="item">The item to be added to the lifetime container.</param>
+        void Add(object item);
+
+        /// <summary>
+        /// Removes an item from the lifetime container. The item is
+        /// not disposed.
+        /// </summary>
+        /// <param name="item">The item to be removed.</param>
+        void Remove(object item);
+
+        /// <summary>
+        /// Determine if a given object is in the lifetime container.
+        /// </summary>
+        /// <param name="item">
+        /// The item to locate in the lifetime container.
+        /// </param>
+        /// <returns>
+        /// Returns true if the object is contained in the lifetime
+        /// container; returns false otherwise.
+        /// </returns>
+        bool Contains(object item);
+    }
+}
