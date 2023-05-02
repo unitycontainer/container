d0d20c5b84e6a9d0942bb0ba71c83f526d7e2d27
ENikS <evgeni@eniks.com>
2023-05-02 10:16:36 -0700

    Adding LFHTScope

---
+++ src/Container/Storage/Scope/LFHTScope/LFHTScope.Internal.cs
@@ -0,0 +1,15 @@
+﻿namespace Unity.Storage
+{
+    public partial class LFHTScope
+    {
+        internal override int IndexOf(Type type, int hash)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override int MoveNext(int index, Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Container/Storage/Scope/LFHTScope/LFHTScope.cs
@@ -0,0 +1,9 @@
+﻿namespace Unity.Storage
+{
+    public partial class LFHTScope : Scope
+    {
+        public LFHTScope(int capacity) : base(capacity)
+        {
+        }
+    }
+}
